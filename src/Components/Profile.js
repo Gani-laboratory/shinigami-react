0a9140a7264cf78b89f46b3a2013522cf8296ca9
QUERY-GANI <ahmadxgani01@gmail.com>
2021-06-18 14:31:08 +0700

    add get by id feature

--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { fetchApi, formatOptions, roles } from "../Util/Utilities"
 
 const Profile = () => {
+    const [users, setUsers] = useState([])
+    useEffect(() => {
+        fetchApi("/users/60c4622b119068608573b81d", "GET").then(val => setUsers(val.data)).catch(e => setUsers(e.data))
+    }, [])
     return (
-        <div>
+        <div className="mt-5">
             <p>Profile Page</p>
+            <ul className="mt-5 mb-5 mx-auto">
+                <li>id: { users._id }</li>
+                <li>role: { roles[users.role] }</li>
+                <li>username: { users.username }</li>
+                <li>email: { users.email }</li>
+                <li>created at: { new Date(users.createdAt).toLocaleDateString("id", formatOptions()) }</li>
+                <li>updated at: { new Date(users.updatedAt).toLocaleDateString("id", formatOptions()) }</li>
+            </ul>
         </div>
     )
 }
