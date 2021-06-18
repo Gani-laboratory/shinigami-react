da3d882e363c702621c619e6c3bb12dab7ec98d1
QUERY-GANI <ahmadxgani01@gmail.com>
2021-06-18 16:46:49 +0700

    add feature route

---
+++ src/Components/Features.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Features = () => {
+    return (
+        <div className="mt-5">
+            <p>Swagger Page</p>
+        </div>
+    )
+}
+
+export default Features;
