f2c08e8f1c415799314e049b1f054d5a456a859a
QUERY-GANI <ahmadxgani01@gmail.com>
2021-06-18 11:27:13 +0700

    move request rest api to Utilities

--- src/Components/ListUser.js
+++ src/Components/ListUser.js
@@ -1,35 +1,29 @@
 import React, { useEffect, useState } from "react";
-import env from "react-dotenv";
-import axios from "axios";
-import { formatOptions } from "../Util/Utilities"
+import { fetchApi, formatOptions } from "../Util/Utilities"
 
 const User = () => {
     const [users, setUsers] = useState([])
     const role = ["", "owner", "admin", "member"]
     useEffect(() => {
-        axios({
-            url: `${env.API_URI}/users`,
-            method: "GET",
-            headers: {
-                "x-apikey": env.API_KEY
-            }
-        }).then(val => setUsers(val.data.users)).catch(e => setUsers(e.data))
+        fetchApi("/users", "GET").then(val => setUsers(val.data.users)).catch(e => setUsers(e.data))
     }, [])
     return (
         <div className="mt-5">
             <p>User page</p>
-            { users.map(v => {
-                return (
-                    <ul key={v._id} className="mt-5 mb-5 mx-auto">
-                        <li>id: { v._id }</li>
-                        <li>role: { role[v.role] }</li>
-                        <li>username: { v.username }</li>
-                        <li>email: { v.email }</li>
-                        <li>created at: { new Date(v.createdAt).toLocaleDateString("id", formatOptions()) }</li>
-                        <li>updated at: { new Date(v.updatedAt).toLocaleDateString("id", formatOptions()) }</li>
-                    </ul>
-                )
-            }) }
+            {
+                users.map(v => {
+                    return (
+                        <ul key={v._id} className="mt-5 mb-5 mx-auto">
+                            <li>id: { v._id }</li>
+                            <li>role: { role[v.role] }</li>
+                            <li>username: { v.username }</li>
+                            <li>email: { v.email }</li>
+                            <li>created at: { new Date(v.createdAt).toLocaleDateString("id", formatOptions()) }</li>
+                            <li>updated at: { new Date(v.updatedAt).toLocaleDateString("id", formatOptions()) }</li>
+                        </ul>
+                    )
+                })
+            }
         </div>
     )
 }
--- src/Util/Utilities.js
+++ src/Util/Utilities.js
@@ -1,5 +1,18 @@
+import axios from "axios";
+import env from "react-dotenv";
+
 const formatOptions = (options = {}) => {
     return {  weekday: "long", year: "numeric", month: "long", day: "numeric", ...options }
 }
+const fetchApi = (endPoint, method, options = {}) => {
+    return axios({
+        url: `${env.API_URI}${endPoint}`,
+        method,
+        headers: {
+            "x-apikey": env.API_KEY
+        },
+        ...options
+    })
+}
 
-export { formatOptions }
+export { formatOptions, fetchApi }
