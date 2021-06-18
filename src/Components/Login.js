5cc05817c126c3fa20783b1ff0bdbe69bb8a4028
QUERY-GANI <ahmadxgani01@gmail.com>
2021-06-18 21:53:18 +0700

    add register feature

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import swal from "sweetalert2";
+import Swal from "sweetalert2";
 import { fetchApi } from "../Util/Utilities";
 
 const Login = () => {
@@ -23,7 +23,7 @@ const Login = () => {
     const onSubmit = (event) => {
         event.preventDefault()
         if (!name || !password) {
-            swal.fire({
+            Swal.fire({
                 text: "Semua input wajib diisi",
                 title: "Gagal Login",
                 icon: "error",
@@ -32,13 +32,13 @@ const Login = () => {
             fetchApi("/login", "POST", {
                 data: JSON.stringify({ name, password })
             }, { "Content-Type": "application/json" }).then(v => {
-                swal.fire({
+                Swal.fire({
                     text: v.data.message,
                     title: "Berhasil Login",
                     icon: "success",
                 })
             }).catch(e => {
-                swal.fire({
+                Swal.fire({
                     text: e,
                     title: "Gagal login",
                     icon: "error"
@@ -49,11 +49,9 @@ const Login = () => {
     return (
         <div className="mt-5">
             <p>Login Page</p>
-            <form>
-                <input onChange={onChange} value={name} id="name" placeholder="Please input your username or email" />
-                <input type="password" onChange={onChange} value={password} id="password" placeholder="Please input your password" />
-                <button type="submit" onClick={onSubmit}>Login</button>
-            </form>
+            <input onChange={onChange} value={name} id="name" placeholder="Please input your username or email" />
+            <input type="password" onChange={onChange} value={password} id="password" placeholder="Please input your password" />
+            <button type="submit" onClick={onSubmit}>Login</button>
         </div>
     )
 }
--- src/Components/Register.js
+++ src/Components/Register.js
@@ -1,9 +1,75 @@
-import React from "react";
+import React, { useState } from "react";
+import Swal from "sweetalert2";
+import { fetchApi } from "../Util/Utilities";
 
 const Register = () => {
+    const [username, setUsername] = useState("")
+    const [email, setEmail] = useState("")
+    const [pw, setPw] = useState("")
+    const [confirm, setConfirm] = useState("")
+    
+    const onChange = (event) => {
+        event.preventDefault()
+        const val = event.target.value
+        switch (event.target.id) {
+            case "username":
+                setUsername(val)
+                break;
+            case "email":
+                setEmail(val)
+                break;
+            case "pw":
+                setPw(val)
+                break;
+            case "confirm":
+                setConfirm(val)
+                break;
+            default:
+                break;
+        }
+    }
+    const onSubmit = (event) => {
+        event.preventDefault()
+        if (!username || !pw || !confirm || !email) {
+            Swal.fire({
+                text: "Semua input wajib diisi",
+                title: "Gagal Register",
+                icon: "error",
+              })
+        } else {
+            if (pw === confirm) {
+                fetchApi("/users", "POST", {
+                    data: JSON.stringify({ username, password: pw, email })
+                }, { "Content-Type": "application/json" }).then(v => {
+                    Swal.fire({
+                        text: v.data.message,
+                        title: "Berhasil Register",
+                        icon: "success",
+                    })
+                }).catch(e => {
+                    Swal.fire({
+                        text: e,
+                        title: "Gagal Register",
+                        icon: "error"
+                    })
+                })
+            } else {
+                Swal.fire({
+                    text: "Password tidak sama dengan confirm password",
+                    title: "Gagal Register",
+                    icon: "error",
+                })
+            }
+        }
+    }
     return (
         <div className="mt-5">
             <p>Register Page</p>
+            <input type="text" onChange={onChange} value={username} id="username" placeholder="Please input your username" />
+            <input type="email" onChange={onChange} value={email} id="email" placeholder="Please input your email" />
+            <input type="password" onChange={onChange} value={pw} id="pw" placeholder="Please input your password" />
+            <input type="password" onChange={onChange} value={confirm} id="confirm" placeholder="Please input your confirm password" />
+            <button type="submit" onClick={onSubmit}>Register</button>
         </div>
     )
 }
