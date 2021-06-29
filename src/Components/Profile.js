fbe6d1a1700e3798ed93d8f14d9d513e3734b38b
QUERY-GANI <ahmadxgani01@gmail.com>
2021-06-29 14:41:28 +0700

    edit view & add font awesome

--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -34,15 +34,15 @@ const Profile = () => {
     }
     return (
         <div className="flex absolute w-full h-full bg-gray-900 text-gray-50">
-            <div className="flex flex-col m-auto justify-between p-5 rounded-md bg-gray-700 w-1/2 h-1/2">
+            <div className="flex flex-col m-auto justify-between p-5 rounded-md bg-gray-700 md:w-1/2 w-11/12 h-1/2">
                 <div className="self-center font-bold text-xl">
                     <h1>User Profile Page</h1>
                 </div>
-                <div className="self-center bg-gray-600 p-5">
-                    <p className="text-lg font-medium">{ GlobalState.user.email }</p>
+                <div className="self-center bg-gray-600 lg:p-5 p-3 rounded">
+                    <p className="md:text-lg text-base font-medium">{ GlobalState.user.email }</p>
                     <p className="text-xs font-thin text-gray-300">{ GlobalState.user._id }</p>
                 </div>
-                <div className="flex justify-around bg-gray-600 py-5 w-1/2 self-center">
+                <div className="flex justify-around bg-gray-600 lg:p-5 p-3 rounded md:w-8/12 w-11/12 self-center">
                     <div>
                         <p className="text-sm font-medium">Created at</p>
                         <p className="text-xs font-thin">{ new Date(GlobalState.user.createdAt).toLocaleDateString("id", formatOptions()) }</p>
@@ -52,16 +52,16 @@ const Profile = () => {
                         <p className="text-xs font-thin">{ new Date(GlobalState.user.updatedAt).toLocaleDateString("id", formatOptions()) }</p>
                     </div>
                 </div>
-                <div className="flex justify-around bg-gray-600 py-5">
-                    <Link className="bg-indigo-600 rounded p-2 border border-indigo-800" to="/edit">
+                <div className="flex md:justify-around justify-between bg-gray-600 lg:p-5 p-3 rounded">
+                    <Link className="bg-indigo-600 rounded md:p-2 p-1 text-center border border-indigo-700" to="/edit">
                         Edit account
                     </Link>
                     { GlobalState.user.role === "ADMIN" && (
-                    <Link className="bg-indigo-600 rounded p-2 border border-indigo-800" to="/user">
-                        List user
+                    <Link className="bg-indigo-600 md:p-2 p-1 text-center border border-indigo-700 rounded" to="/user">
+                        <i className="fad fa-users-cog"></i>
                     </Link>
                     ) }
-                    <button className="bg-indigo-600 rounded p-2 border border-indigo-800 focus:outline-none" onClick={deleteAccount}>
+                    <button className="bg-indigo-600 rounded md:p-2 p-1 text-center border border-indigo-700 focus:outline-none" onClick={deleteAccount}>
                         Delete account
                     </button>
                 </div>
